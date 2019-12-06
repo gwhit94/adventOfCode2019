0da8190799b6be9cee8fbbfd492ab1d42e65c926
Graham <greysnaa@gmail.com>
2019-12-06 04:11:09 -0600

    Day 2 Part 2 solved!

--- Day2/intcode.js
+++ Day2/intcode.js
@@ -5,14 +5,17 @@
 ////for example, 99 means that the program is finished and should immediately halt. 
 //Encountering an unknown opcode means something went wrong.
 
-// Program input [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,13,27,1,10,27,31,2,31,13,35,1,10,35,39,2,9,39,43,2,43,9,47,1,6,47,51,1,10,51,55,2,55,13,59,1,59,10,63,2,63,13,67,2,67,9,71,1,6,71,75,2,75,9,79,1,79,5,83,2,83,13,87,1,9,87,91,1,13,91,95,1,2,95,99,1,99,6,0,99,2,14,0,0]
-var intcode = [];
+// Program input [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,13,27,1,10,27,31,2,31,13,35,1,10,35,39,2,9,39,43,2,43,9,47,1,6,47,51,1,10,51,55,2,55,13,59,1,59,10,63,2,63,13,67,2,67,9,71,1,6,71,75,2,75,9,79,1,79,5,83,2,83,13,87,1,9,87,91,1,13,91,95,1,2,95,99,1,99,6,0,99,2,14,0,0]
+// Program input pt1 [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,13,27,1,10,27,31,2,31,13,35,1,10,35,39,2,9,39,43,2,43,9,47,1,6,47,51,1,10,51,55,2,55,13,59,1,59,10,63,2,63,13,67,2,67,9,71,1,6,71,75,2,75,9,79,1,79,5,83,2,83,13,87,1,9,87,91,1,13,91,95,1,2,95,99,1,99,6,0,99,2,14,0,0]
+var intcode = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,13,27,1,10,27,31,2,31,13,35,1,10,35,39,2,9,39,43,2,43,9,47,1,6,47,51,1,10,51,55,2,55,13,59,1,59,10,63,2,63,13,67,2,67,9,71,1,6,71,75,2,75,9,79,1,79,5,83,2,83,13,87,1,9,87,91,1,13,91,95,1,2,95,99,1,99,6,0,99,2,14,0,0];
 let i = 0;
 var opcode = 0;
 let x;
 let y;
 let z;
 let a;
+let processCounter = document.getElementById('processes');
+
 // opcode = 1 --> adds together numbers read from two positions and stores the result in a third position.
 // The three integers immediately after the opcode tell you these three positions
 // - the first two indicate the positions from which you should read the input values,
@@ -44,11 +47,50 @@ function computer(){
             break;
         case 99:
             i = 0;
-            console.log(intcode);
+            // console.log(intcode);
             break;
             
         default:
-            console.log(intcode);
+            // console.log(intcode);
             break;
     }
 };
+
+function memoryReset(){
+    intcode = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,13,27,1,10,27,31,2,31,13,35,1,10,35,39,2,9,39,43,2,43,9,47,1,6,47,51,1,10,51,55,2,55,13,59,1,59,10,63,2,63,13,67,2,67,9,71,1,6,71,75,2,75,9,79,1,79,5,83,2,83,13,87,1,9,87,91,1,13,91,95,1,2,95,99,1,99,6,0,99,2,14,0,0];
+    return;
+}
+function inputCheck(){
+    if (intcode[0] == 19690720){
+        console.log(`
+            ${intcode}
+            Success! ${intcode[0]} 
+            Noun: ${intcode[1]}
+            Verb: ${intcode[2]}`);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function nounverb(){ // I feel like this bit of code could be more eloquent but it worked so I'm happy
+    for (let j = 0; j < 100; j++) {
+        intcode[1] = j;
+        computer();
+        if (inputCheck()){
+            return;
+        } else {
+            memoryReset();
+            for (let k = 0; k < 100; k++) {
+                intcode[1] = j;
+                intcode[2] = k;
+                computer();
+                if (inputCheck()){
+                    return;
+                } else {
+                    memoryReset();
+                };
+            };
+        };
+    }
+}
